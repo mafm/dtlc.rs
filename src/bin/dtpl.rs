5b9ee5b9aee9bd435ea13058a014ff36c3999b83
Darin Morrison <darinmorrison+git@gmail.com>
2014-10-12 15:38:03 -0600

    Put examples in a mod

--- src/bin/dtpl.rs
+++ src/bin/dtpl.rs
@@ -1,62 +1,64 @@
 extern crate dtpl;
 
-use dtpl::core::eval;
-use dtpl::core::model::{
-    nrm,
-};
-use dtpl::core::syntax::{
-    chk,
-    inf,
-    sym,
-    typ,
-};
-use dtpl::parse;
+mod example {
+    use dtpl::core::eval;
+    use dtpl::core::model::{
+        nrm,
+    };
+    use dtpl::core::syntax::{
+        chk,
+        inf,
+        sym,
+        typ,
+    };
+    use dtpl::parse;
 
-fn example_eval() {
-    println!("evaluating:\n\t\"((\\x. x) : A -> A) welp\"");
-    let term_orig: chk::Chk =
-        chk::Inf(
-            box inf::App(
-                box inf::Ann(
-                    box chk::Abs(
-                        box chk::Inf(
-                            box inf::Var(0)
-                        )
+    pub fn eval() {
+        println!("evaluating:\n\t\"((\\x. x) : A -> A) welp\"");
+        let term_orig: chk::Chk =
+            chk::Inf(
+                box inf::App(
+                    box inf::Ann(
+                        box chk::Abs(
+                            box chk::Inf(
+                                box inf::Var(0)
+                            )
+                        ),
+                        box typ::Fun(
+                            box typ::Par(sym::Con(String::from_str("A"))),
+                            box typ::Par(sym::Con(String::from_str("A"))),
+                        ),
                     ),
-                    box typ::Fun(
-                        box typ::Par(sym::Con(String::from_str("A"))),
-                        box typ::Par(sym::Con(String::from_str("A"))),
+                    box chk::Inf(
+                        box inf::Par(sym::Con(String::from_str("welp")))
                     ),
-                ),
-                box chk::Inf(
-                    box inf::Par(sym::Con(String::from_str("welp")))
-                ),
-            )
-        );
-    let term_eval: nrm::Nrm = eval::chk(term_orig, Vec::new());
-    println!("result:\n\t{}", term_eval);
-}
+                )
+            );
+            let term_eval: nrm::Nrm = eval::chk(term_orig, Vec::new());
+            println!("result:\n\t{}", term_eval);
+        }
 
-fn example_parse() {
-    println!("parsing:\n\t\"42\"")
-    let sym: Box<sym::Sym> = box sym::Con(String::from_str("42"));
-    let res: Result<Box<sym::Sym>,String> = parse::sym("42");
-    assert_eq!(res, Ok(sym));
-    println!("parsed:\n\t{}", res);
-}
+    pub fn parse() {
+        println!("parsing:\n\t\"42\"")
+        let sym: Box<sym::Sym> = box sym::Con(String::from_str("42"));
+        let res: Result<Box<sym::Sym>,String> = parse::sym("42");
+        assert_eq!(res, Ok(sym));
+        println!("parsed:\n\t{}", res);
+    }
 
-fn example_print() {
-    let ty_par: Box<typ::Typ> = box typ::Par(sym::Con(String::from_str("A")));
-    let ty_fun: Box<typ::Typ> = box typ::Fun(ty_par.clone(), ty_par);
-    let tm_chk: Box<chk::Chk> = box chk::Abs(box chk::Inf(box inf::Var(0)));
-    println!("printing:\n\t\"A -> A\":\n\t\t{}", ty_fun);
-    println!("printing:\n\t\"\\x. x\":\n\t\t{}", tm_chk);
+    pub fn render() {
+        let ty_par: Box<typ::Typ> = box typ::Par(sym::Con(String::from_str("A")));
+        let ty_fun: Box<typ::Typ> = box typ::Fun(ty_par.clone(), ty_par);
+        let tm_chk: Box<chk::Chk> = box chk::Abs(box chk::Inf(box inf::Var(0)));
+        println!("printing:\n\t\"A -> A\":\n\t\t{}", ty_fun);
+        println!("printing:\n\t\"\\x. x\":\n\t\t{}", tm_chk);
+    }
 }
 
 fn main() {
-    example_eval();
+    example::eval();
     println!("\n---\n");
-    example_parse();
+    example::parse();
     println!("\n---\n");
-    example_print();
+    example::render();
 }
