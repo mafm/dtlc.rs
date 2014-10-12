162c04b5bc43520efd9fb7307b702ae478881dce
Darin Morrison <darinmorrison+git@gmail.com>
2014-10-12 15:46:05 -0600

    Refactor: simplify core mod

--- src/dtpl/core/mod.rs
+++ src/dtpl/core/mod.rs
@@ -1,18 +1,3 @@
-pub use self::eval::{
-};
-
-pub use self::model::{
-    neu,
-    nrm,
-};
-
-pub use self::syntax::{
-    sym,
-    chk,
-    inf,
-    typ,
-};
-
 pub mod eval;
 pub mod model;
 pub mod syntax;
