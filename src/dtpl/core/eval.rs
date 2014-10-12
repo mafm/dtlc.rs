2438992101c5368a5a71b3b2e597de63726b2e9a
Darin Morrison <darinmorrison+git@gmail.com>
2014-10-12 15:34:30 -0600

    Refactor: more cleanup

--- src/dtpl/core/eval.rs
+++ src/dtpl/core/eval.rs
@@ -16,28 +16,23 @@ fn vpar(n:sym::Sym,) -> nrm::Nrm {
 
 fn vapp(v1:nrm::Nrm, v2:nrm::Nrm) -> nrm::Nrm {
     match v1 {
-        nrm::Abs(box v1c, mut v1e) => {
-            v1e.push(v2);
-            chk(v1c, v1e)
-        },
-        nrm::Neu(v1n) => {
-            nrm::Neu(box neu::App(v1n, box v2))
-        },
+        nrm::Abs(box v1c, mut v1e) => { v1e.push(v2); chk(v1c, v1e) },
+        nrm::Neu(v1n)              => { nrm::Neu(box neu::App(v1n, box v2)) },
     }
 }
 
 pub fn chk(c:chk::Chk, e:Env) -> nrm::Nrm {
     match c {
-        chk::Inf(box ci) => inf(ci, e),
-        chk::Abs(cc)     => nrm::Abs(cc, e),
+        chk::Inf(box ci) => { inf(ci, e) },
+        chk::Abs(cc)     => { nrm::Abs(cc, e) },
     }
 }
 
 fn inf(i:inf::Inf, e:Env) -> nrm::Nrm {
     match i {
-        inf::Ann(box ic, _)      => chk(ic, e),
-        inf::App(box ii, box ic) => vapp(inf(ii, e.clone()), chk(ic, e)),
-        inf::Par(ix)             => vpar(ix),
-        inf::Var(iu)             => e[iu].clone(),
+        inf::Ann(box ic, _)      => { chk(ic, e) },
+        inf::App(box ii, box ic) => { vapp(inf(ii, e.clone()), chk(ic, e)) },
+        inf::Par(ix)             => { vpar(ix) },
+        inf::Var(iu)             => { e[iu].clone() },
     }
 }
