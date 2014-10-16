d51c3adf2ef4e83cbaf20ab40ecab1e142bdfc99
Darin Morrison <darinmorrison+git@gmail.com>
2014-10-16 00:36:07 -0600

    Add println to normal; commented by default

--- src/dtpl/core/normal.rs
+++ src/dtpl/core/normal.rs
@@ -19,13 +19,20 @@ mod sem {
     };
 
     pub fn par(n:sym::Sym) -> nrm::Nrm {
+        // println!("par :: n={}", n)
         nrm::Neu(box neu::Par(n))
     }
 
     pub fn app(v1:nrm::Nrm, v2:nrm::Nrm) -> nrm::Nrm {
         match v1 {
-            nrm::Abs(box v1c, mut v1e) => { v1e.push(v2); chk(v1c, v1e) },
-            nrm::Neu(v1n) => { nrm::Neu(box neu::App(v1n, box v2)) },
+            nrm::Abs(box v1c, mut v1e) => {
+                // println!("app :: Abs  |\n\t\tv1c = {}\n\t\tv1e = {}\n\t\t v2 = {}", v1c, v1e, v2);
+                v1e.push(v2); chk(v1c, v1e)
+            },
+            nrm::Neu(v1n) => {
+                // println!("app :: Neu  |\n\t\tv1n = {}\n\t\t v2 = {}", v1n, v2);
+                nrm::Neu(box neu::App(v1n, box v2))
+            },
         }
     }
 }
@@ -37,16 +44,34 @@ pub fn lookup(i:uint, e:nrm::Env) -> nrm::Nrm {
 
 pub fn chk(c:chk::Chk, e:nrm::Env) -> nrm::Nrm {
     match c {
-        chk::Inf(box ci) => { inf(ci, e) },
-        chk::Abs(cc) => { nrm::Abs(cc, e) },
+        chk::Inf(box ci) => {
+            // println!("chk :: Inf  |\n\t\t  e = {}\n\t\t ci = {}", e, ci);
+            inf(ci, e)
+        },
+        chk::Abs(cc) => {
+            // println!("chk :: Abs  |\n\t\t  e = {}\n\t\t cc = {}", e, cc);
+            nrm::Abs(cc, e)
+        },
     }
 }
 
 fn inf(i:inf::Inf, e:nrm::Env) -> nrm::Nrm {
     match i {
-        inf::Ann(box ic, _) => { chk(ic, e) },
-        inf::App(box ii, box ic) => { sem::app(inf(ii, e.clone()), chk(ic, e)) },
-        inf::Par(ix) => { sem::par(ix) },
-        inf::Var(iu) => { lookup(iu, e) },
+        inf::Ann(box ic, _) => {
+            // println!("inf :: Ann  |\n\t\t  e = {}\n\t\t ic = {}", e, ic);
+            chk(ic, e)
+        },
+        inf::App(box ii, box ic) => {
+            // println!("inf :: App  |\n\t\t  e = {}\n\t\t ii = {}\n\t\t ic = {}", e, ii, ic);
+            sem::app(inf(ii, e.clone()), chk(ic, e))
+        },
+        inf::Par(ix) => {
+            // println!("inf :: Par  |\n\t\t  e = {}\n\t\t ix = {}", e, ix);
+            sem::par(ix)
+        },
+        inf::Var(iu) => {
+            // println!("inf :: Var  |\n\t\t  e = {}\n\t\t iu = {}", e, iu);
+            lookup(iu, e)
+        },
     }
 }
